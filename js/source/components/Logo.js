59d5652e7f3160130497f78cb65e771d0ac520a0
Byeong-Hak, Lee <pro87kr@hanmail.net>
2018-02-06 15:53:39 +0900

    1단계 : 초기 앱 개발을 위한 설정

---
+++ js/source/components/Logo.js
@@ -0,0 +1,19 @@
+'use strict'
+//var React = require('react');
+import React from 'react';
+
+// var Logo = React.createClass({
+//   render: function(){
+//     return <div className="Logo" />;
+//   }
+// })
+class Logo extends React.Component {
+  render(){
+    return <div className="Logo" />
+  }
+}
+
+
+// module.exports = Logo;
+export default Logo
+
